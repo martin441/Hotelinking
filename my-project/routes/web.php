f115ba54f2dabfd8dc95f86b7501ed1def212140
Martin Machado <martinmachado441@gmail.com>
2023-05-08 17:42:10 -0300

    vista promociones

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -37,8 +37,11 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/promotions', [PromotionController::class, 'index'])->middleware(['auth', 'verified'])->name('promotions.index');
+    Route::get('/promotions', [PromotionController::class, 'index'])
+        ->middleware(['auth', 'verified'])
+        ->name('promotions.index');
 });
+
 
 
 require __DIR__.'/auth.php';
